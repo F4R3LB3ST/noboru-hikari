42e2de31faa3f6648c5ac4d1e56077d58bfd4fef
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-03-25 09:07:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ client.on('message', message => {
       console.log("msgSp = " + msgSp)
       for (var j = 0; j <= greet.length; j++) {
         if (msgSp[i] == greet[j]) {
+          break;
         message.channel.send("Hi " + sender);
         break;
       };
