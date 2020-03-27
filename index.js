273bdded436ebbfe6bf6c2c2b08e79930f1c0019
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-03-28 00:30:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ client.on('message', message => {
         message.channel.send("pong!")
       } else if (msglow.includes("cov")) {
         message.channel.send("instead of making jokes about SARS-2, why you guys not donate to the charity to help the healthcare")
-        setTimeout(function(){
-          .then ((message)=>{
-            message.edit("DONATE NOW!")
+        setTimeout(function(msg){
+          .then ((msg)=>{
+            msg.edit("DONATE NOW!")
           })
         }, 1000)
         message.channel.send("https://www.globalgiving.org/projects/coronavirus-relief-fund/")
