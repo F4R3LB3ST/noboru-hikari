923872865071bb10803b98cc005496f905f59b18
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-03-28 20:14:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ client.on('message', message => {
               message.channel.send("mention 2 members and separate it with ' | ', example : 'n-jvd Jotaro | Dio'")
           }*/
         };
-      };
-  });
+      });
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'member-log');
