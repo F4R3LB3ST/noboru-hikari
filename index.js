7403de8fd60a34cfc8e59e080c6a00820848bed6
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-05-04 08:02:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,9 @@ client.on('message', message => {
       if (msglow.startsWith("n-")) {
         message.channel.send("can't accept commands, re-enable it with n-smart")
       };
-    });
+    };
+};
+});
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'member-log');
