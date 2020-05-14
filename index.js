f9f1c9c11abc07687ee771b88c3883d2aebd6e1c
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-05-14 23:52:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,9 @@ client.on('message', message => {
               }
             }
           } else if (greet.includes(msgsplit[a])) {
-            message.channel.send(greet[Math.floor(Math.random() * 5)] + message.author.username);
+            message.channel.send(`${greet[Math.floor(Math.random() * 5)]}  ${message.author.username}`);
+            done = true
+            break;
           } else if (done) {
             message.channel.send("Sorry, i couldn't understand, maybe i can't read your sentence");
             break;
