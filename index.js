4d864b6a5e4ff56c27f1866b6ae284861352c764
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-05-15 20:12:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ client.on('ready', () => {
 
 function Capitalize(string) {
   var fletter = string.charAt().toUpperCase();
+  console.log(fletter);
   var lletter = string.slice(1);
   string =  fletter.concat(lletter);
   return string;
