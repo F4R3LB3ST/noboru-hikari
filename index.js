67fb4c004457ba58925f3fb29f05ee6c82d6a58a
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-05-16 04:08:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const guild = new Discord.Guild();
 var fs = require('fs');
 const command = require('./command.json');
 const smart = require('./smart.json')
